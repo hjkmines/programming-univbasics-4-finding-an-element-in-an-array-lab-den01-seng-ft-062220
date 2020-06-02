cca7396e5a522ed0dd9076fe5050703266d5c71c
Hyung Kim <hjkmines@gmail.com>
2020-06-02 01:03:49 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,10 +4,11 @@ def find_element_index(array, value_to_find)
   while counter < array.length
     if array[counter] = value_to_find
       return array.index[value_to_find]
+      counter += 1
     else 
       nil 
     end
-  counter += 1 
-end 
+  
+  end 
   
 end
